062bc06cdaa83407042dcaade465a9306d409e97
enescagrihan <enescagrihan@outlook.com>
2023-12-23 13:31:11 +0300

    refactor: Add navbar, header and set layout.

---
+++ components/shared/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return (
+        <div className="flex self-center gap-10">
+            <button>List</button>
+            <button>Home</button>
+            <button>Exercise</button>
+        </div>
+    )
+}
