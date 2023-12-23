2f58d5ca2e1b4a148f74dd8082d7517ed1388531
enescagrihan <enescagrihan@outlook.com>
2023-12-23 14:25:17 +0300

    refactor: Change logo and add setting page icon in header.

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,3 +1,14 @@
+import Image from "next/image";
+import Logo from "@/public/logo.svg";
+import Settings from "@/public/settings.svg";
+
 export default function Header() {
-  return <header className="font-mono text-xl">VocabHelp</header>;
+  return (
+    <header className="font-mono text-xl flex justify-between">
+      <Image src={Logo} alt="Logo" />
+      <button>
+        <Image src={Settings} alt="Logo" />
+      </button>
+    </header>
+  );
 }
